89bf5b0fbb297c787e528b32aa57ec9ef3ec13eb
hyalen <hyalen.neves@toroinvestimentos.com.br>
2021-01-13 23:32:56 -0300

    Created the data model for the SQL queries

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -0,0 +1,9 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Files {
+    private Integer fileId;
+    private Integer userId;
+    private String fileName;
+    private String contentType;
+    private String fileSize;
+}
