dba52e3f8661f98bb17bd3511b55df5876f22ada
hyalen <hyalen.neves@toroinvestimentos.com.br>
2021-01-16 15:51:11 -0300

    Modified the templates and created model/services logic for the Login, Signup and Home pages

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfiguration.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /*
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
         * enables HTTP Basic and Form based authentication
         * Spring Security will automatically render a login page and logout success page for you
  */
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     AuthenticationService authenticationService;
 
@@ -25,15 +29,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/signup", "/css/**", "/js/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated();
-
-        http.formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .defaultSuccessUrl("/chat", true);;
+        http
+            .authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/home", true)
+                .and()
+            .logout(logout -> logout
+                .permitAll()
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/login?logout")
+            );
     }
 }
